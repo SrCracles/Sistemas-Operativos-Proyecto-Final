c19b3cbc01865ac6dee8f3878ce826d1de3bb9aa
Alejandro Mejía <Alejandroamm1432@gmail.com>
2025-10-20 16:28:59 -0500

    Top 10 Archivos mas Grandes (Punto 3)

---
+++ Powershell/files.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host "`n=== TOP 10 LARGEST FILES ==="
+
+$drive = Read-Host "Enter drive (example: C:)"
+
+if (-not (Test-Path $drive)) {
+    Write-Host "Drive does not exist"
+    return
+}
+
+$files = Get-ChildItem -Path $drive -Recurse -File | Sort-Object Length -Descending | Select-Object -First 10
+
+$results = @()
+foreach ($file in $files) {
+    $results += [PSCustomObject]@{
+        FullPath = $file.FullName
+        SizeBytes = $file.Length
+    }
+}
+
+$results | Format-Table -AutoSize
