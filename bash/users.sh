caf9432885cfe3ebb2935c6aaa7a5f1317045f3e
Alejandro Mejía <Alejandroamm1432@gmail.com>
2025-11-08 16:37:49 -0500

    Users (punto 1) Actualizado

--- bash/users.sh
+++ bash/users.sh
@@ -4,7 +4,7 @@ echo ""
 echo "=== SYSTEM USERS ==="
 echo ""
 
-awk -F: '{print $1}' /etc/passwd | while read user
+awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | while read user
 do
     lastlogin=$(last -1 $user | head -1 | awk '{if (NF > 3) print $4, $5, $6, $7; else print "Never"}')
 
